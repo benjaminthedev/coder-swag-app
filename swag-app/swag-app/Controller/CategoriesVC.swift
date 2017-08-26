d82ef28f905ef678484e6ec3bce7b704c0826df6
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-26 08:29:43 +0100

    Inital set up and stage one of project with all assets

---
+++ swag-app/swag-app/Controller/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swag-app
+//
+//  Created by Benjamin on 25/08/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
