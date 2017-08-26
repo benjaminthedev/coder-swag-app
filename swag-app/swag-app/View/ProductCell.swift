13e51956026a89528feb494e4b79210b80a46646
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-08-26 14:47:01 +0100

    Add all the views in this section next up is the data

---
+++ swag-app/swag-app/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  swag-app
+//
+//  Created by Benjamin on 26/08/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
